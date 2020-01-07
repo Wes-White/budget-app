a8db984f195c67770c1c8dce9289b050ac74084a
Wes-White <wesleyrwhite86@gmail.com>
2020-01-06 16:02:21 -1000

    Updated bp with CRA

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
